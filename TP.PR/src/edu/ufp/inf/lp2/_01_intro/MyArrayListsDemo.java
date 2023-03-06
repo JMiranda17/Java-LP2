f9b1d49bbcdb96282b109911b189b0b942f5c27c
JMiranda <jmirandaa17@gmail.com>
2023-03-06 15:41:09 +0000

    MyArraysDemo & MyArrayListsDemo -Differences between the two; -When to use one and another; -Examples

--- TP.PR/src/edu/ufp/inf/lp2/_01_intro/MyArrayListsDemo.java
+++ TP.PR/src/edu/ufp/inf/lp2/_01_intro/MyArrayListsDemo.java
@@ -9,7 +9,7 @@ public class MyArrayListsDemo {
         arrList1.add(new Object());
         arrList1.add("OLA");
         arrList1.add(new Point(1.0f,1.0f));
-        arrList1.add("teste");
+        arrList1.add(3,"OLA NOVAMENTE");
         for (Object o : arrList1){
             System.out.println(o.toString());
         }
